02d4363557de99e2f56d822a44e045c3154c5e14
Kane Piggott <91965676+Kane675@users.noreply.github.com>
2022-05-30 12:45:42 +0100

    Player with tool

--- Don't Get Over It/Assets/Scripts/CamLock.cs
+++ Don't Get Over It/Assets/Scripts/CamLock.cs
@@ -27,8 +27,17 @@ public class CamLock : MonoBehaviour
     {
         X = player.transform.position.x;
         Y = player.transform.position.y;
-    
-        transform.position = new Vector3(X, Y, -10);
-   
+        if (Y < -3)
+        {
+            transform.position = new Vector3(X, -3, -10);
+        }
+        if (X<0)
+        {
+            transform.position = new Vector3(0, Y, -10);
+        }
+        else
+        {
+            transform.position = new Vector3(X, Y, -10);
+        }
     }
 }
